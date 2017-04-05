3550796e20eda5c25a68c20e0f6e85eeec8bee68
ciaran-dk <ciaran@murraynet.dk>
2017-04-05 14:40:07 +0200

    v2

--- server.R
+++ server.R
@@ -5,6 +5,12 @@ library(shiny)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
    # missing code
-    
+  outText <- eventReactive(input$goButton, {
+    input$n
+  })
+  
+  output$nText <- renderText({
+    outText()
+  })
   })
   
--- ui.R
+++ ui.R
@@ -1,18 +1,35 @@
 library(shiny)
 
 shinyUI(fluidPage(title="WATERS",
-  
-  titlePanel(
-    a(href="http://waters.gu.se", target="_blank", img(src="1530704_waters_webheader2.jpg"))),
-  
-  navlistPanel(
-    "Header A",
-    tabPanel("Component 1"),
-    tabPanel("Component 2"),
-    "Header B",
-    tabPanel("Component 3"),
-    tabPanel("Component 4"),
-    "-----",
-    tabPanel("Component 5")
-  )
-))
+                  
+                  titlePanel(
+                    a(href="http://waters.gu.se", target="_blank", img(src="1530704_waters_webheader2.jpg"))),
+                  
+                  navlistPanel(
+                    "Input",
+                    tabPanel("Data",
+                             actionButton("goButton", "Get data"),
+                             fileInput('file1', 'Load existing assessment',
+                                       accept=c('text/csv', 
+                                                'text/comma-separated-values,text/plain', 
+                                                '.csv'))
+                    ),
+                    "Options",
+                    tabPanel("Fixed and Variance Parameters"),
+                    tabPanel("Indicator Models"),
+                    tabPanel("Boundaries"),
+                    "Results",
+                    tabPanel("Assessment"),
+                    "Output",
+                    tabPanel("Assessment",
+                             fluidRow(
+                               column(2,
+                                      "sidebar"
+                               ),
+                               column(10,
+                                      "main"
+                               )
+                             )
+                    )
+                  ))
+)
