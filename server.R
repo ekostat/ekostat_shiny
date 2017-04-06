6de2e18c3df5b93e8c59e0d7a0c843ac66276952
ciaran-dk <ciaran@murraynet.dk>
2017-04-06 12:32:26 +0200

    still testing...

--- server.R
+++ server.R
@@ -4,7 +4,7 @@ library(shiny)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   # missing code
+  # missing code
   outText <- eventReactive(input$goButton, {
     input$n
   })
@@ -12,5 +12,33 @@ shinyServer(function(input, output) {
   output$nText <- renderText({
     outText()
   })
+  
+  #value <- reactiveValues(0)
+  output$downloadReport <- downloadHandler(
+    contentType="application/pdf",
+    filename = function(){
+      paste0("report-", Sys.Date(), ".pdf")
+    },
+    content = function(file){
+      write("test",file)
+    }
+  )
+  district_list <- reactive({
+    c("xxxx","yyyy","zzzz")
+  })
+  waterbody_list <- reactive({
+    c("Inre Bråviken","Inre Slätbaken","Kaggebofjärden","Yttre Bråviken","Trännöfjärden","Lindödjupet","Kärrfjärden","Ryssbysjön","Stensjön","Vässledasjön","Krön","Lilla Nätaren","Rocksjön","Munksjön","Ören 1","Ören 2","Stora Nätaren","Försjön","Landsjön","Glimmingen","Ralången","Noen","Allgjuttern","Bunn","Säbysjön","Åsunden","Sommen-Västra","Sommen-Östra","Roxen","Vättern - Alsen","Vättern - Storvättern","Vättern - Kärrafjärden","Glan","Fagertärn","Bleklången","Bosjön","Anten","Avern","Skeppsjön","Boren","Bönnern","Hargsjön","Hövern","Asplången","Kisasjön","Nimmern","Drögen")
   })
   
+  output$selectWaterDistrict <- renderUI({
+    tagList(
+      selectInput("district", "Select Water District", choices=district_list(), selected="ALL")
+    )})
+  
+  output$selectWaterBodies <- renderUI({
+    tagList(
+      selectInput("waterbody", "Select Waterbody(s)", choices=waterbody_list(), selected="ALL", multiple=TRUE)
+    )})
+  
+})
+
--- ui.R
+++ ui.R
@@ -1,35 +1,63 @@
 library(shiny)
 
-shinyUI(fluidPage(title="WATERS",
-                  
-                  titlePanel(
-                    a(href="http://waters.gu.se", target="_blank", img(src="1530704_waters_webheader2.jpg"))),
-                  
-                  navlistPanel(
-                    "Input",
-                    tabPanel("Data",
-                             actionButton("goButton", "Get data"),
-                             fileInput('file1', 'Load existing assessment',
-                                       accept=c('text/csv', 
-                                                'text/comma-separated-values,text/plain', 
-                                                '.csv'))
-                    ),
-                    "Options",
-                    tabPanel("Fixed and Variance Parameters"),
-                    tabPanel("Indicator Models"),
-                    tabPanel("Boundaries"),
-                    "Results",
-                    tabPanel("Assessment"),
-                    "Output",
-                    tabPanel("Assessment",
-                             fluidRow(
-                               column(2,
-                                      "sidebar"
-                               ),
-                               column(10,
-                                      "main"
-                               )
-                             )
-                    )
-                  ))
-)
+# Define UI for random distribution application 
+shinyUI(
+  navbarPage(
+    title="WATERS Assessment", 
+    tabPanel("Data",
+             navlistPanel(
+               widths=c(2,10),well=F,
+               tabPanel(
+                 "New",
+                 p(strong("New Assessment")),
+                 p("Select waterbodies and dowload data from database(s)."),
+                 p("Include own data?"),
+                 uiOutput("selectWaterDistrict"),
+                 uiOutput("selectWaterBodies"),
+                 actionButton("goButton", "Download data")
+               ),
+               tabPanel("Load",
+                        fileInput('file1', 'Load existing assessment',
+                                  accept=c('text/csv', 
+                                           'text/comma-separated-values,text/plain', 
+                                           '.csv'))  
+               ),
+               tabPanel("Save",
+                        p(strong("Save the current assessment")),
+                        downloadButton('downloadData', label="Save") 
+               )
+             )
+             
+    ),
+    tabPanel(
+      "Options",
+      navlistPanel(
+        widths=c(2,10),well=F,
+        tabPanel("Uncertainty",
+                 "Uncertainty Library"),
+        tabPanel("Aggregation",
+                 "Aggregation methods"),
+        tabPanel("Monte Carlo",
+                 "Options for Monte Carlo simulations.")
+      )
+    ),
+    
+    tabPanel(
+      "Results",
+      navlistPanel(
+        widths=c(2,10),well=F,
+        tabPanel("Indicator",
+                 "Indicator Results"),
+        tabPanel("Quality Element",
+                 "QE Results"),
+        tabPanel("Overall",
+                 "Overall Results")
+      ),
+      
+      fluidRow(column(width=12,
+        downloadButton('downloadReport', label="Download report")
+      ))
+    )
+  ))
+
+
