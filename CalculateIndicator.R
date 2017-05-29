1acb0170d2870532d77d25a195f082ac3c93848f
ciaran-dk <ciaran@murraynet.dk>
2017-05-29 11:09:41 +0200

    Updates from JAC

--- CalculateIndicator.R
+++ CalculateIndicator.R
@@ -19,23 +19,55 @@
 #' 
 #' @examples
 CalculateIndicator_Chla <-
-  function(df,Indicator_pred_pct = 0.5) {
+  function(df,var_year,var_station,var_res,n_iter=1000,Indicator_pred_pct = 0.5) {
     
 # Add month and year to df
     df <- mutate(df,month=month(date))
     df <- mutate(df,year=month(year))
 # Use only June-August data    
-    df <- filter(df,month %in% 6:8) 
+    df <- filter(df,month %in% 6:8)
+# Remove observations with missing values    
+    df <-drop_na(df,chla)
+# Calculate number of years and stations in df 
+    n_obs <- length(df$chla)
+    n_year <- length(unique(df$year))
+    n_station <- length(unique(df$station))
+# Simulate system with random variables for estimating the variance of the indicator
+    Random_year <- mat.or.vec(n_year, 1)
+    Random_station <- mat.or.vec(n_station, 1)
+    Random_res <- mat.or.vec(n_obs, 1)
+    
+   # simres <- mat.or.vec(n_iter, 1)
+    simres <- vector("numeric",n_iter)
+    for (isim in 1:n_iter) {
+    # simulate variations in the random factors using the data structure
+        for (i in 1:n_year) {Random_year[i] <- rnorm(1) * sqrt(var_year)}
+        for (i in 1:n_station) {Random_station[i] <- rnorm(1) * sqrt(var_station)}
+        for (i in 1:n_obs) {Random_res[i] <- rnorm(1) * sqrt(var_res)}
+        simulvector_year <- Random_year[match(df$year,unique(df$year))]
+        simulvector_station <- Random_station[match(df$station,unique(df$station))]
+        simulvector_obs <- simulvector_year+simulvector_station+Random_res
+        simul_df <- data.frame(df$year,df$station,simulvector_obs)
+        simres[isim] <- simul_df %>%
+          group_by(df.year,df.station) %>%
+          summarise(simulvector_obs = mean(simulvector_obs)) %>%
+          group_by(df.year) %>%
+          summarise(simulvector_obs = mean(simulvector_obs)) %>%
+          summarise(simulvector_obs = mean(simulvector_obs))
+         }
+    
 # Aggregate by year and station    
-    res <- df %>%
+    dfres <- df %>%
       group_by(year,station) %>%
       summarise(chla = mean(chla))
 # Aggregate by year
-    res <- res %>%
+    dfres <- dfres %>%
       group_by(year) %>%
       summarise(chla = mean(chla))
 # Aggregate for entire period
-# Only use indicator if there are at least 3 years of data    
-    res <- ifelse(length(res$chla)>=3,mean(res$chla),na())
+# Only use indicator if there are at least 3 years of data  
+    mean <- ifelse(length(dfres$chla)>=3,mean(dfres$chla),NA)
+    stderr <- ifelse(length(dfres$chla)>=3,sd(as.numeric(simres)),NA)
+    res <- list(mean=mean,stderr=stderr)
     return(res)
 }
--- main.R
+++ main.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(haven)
 library(lme4)
 library(lubridate)
+
 library(extrafont)
 loadfonts(quiet = TRUE)
 theme_set(theme_bw(base_size = 12, base_family = "Open Sans"))
@@ -16,11 +17,14 @@ source("ReadIndicatorParms.R")
 source("CalculateIndicator.R")
 
 # Read data set for specific waterbody and period
-df <- ReadMonitoringDataSMHI("data/Gullmarn_2001_2006.sas7bdat")
+df<-ReadMonitoringDataSMHI("data/Gullmarn_2007_2012.sas7bdat")
+
 # Read general parameters for the indicator
 parmlist <- ReadParms_chla()
 # Calculate the indicator
-CalculateIndicator_Chla(df)
+CalculateIndicator_Chla(df,parmlist$covparams_CumCover$Estimate[parmlist$covparams_CumCover$CovParm == "year(vandomr*period)"],
+                        parmlist$covparams_CumCover$Estimate[parmlist$covparams_CumCover$CovParm == "stati(vandom*period)"],
+                        parmlist$covparams_CumCover$Estimate[parmlist$covparams_CumCover$CovParm == "Residual"])
 
 
 
